e914fe7d5e8b399e6299d98768163c1d82d9eec1
Mehmet Ali Duran <mehmetali.duran072002@gmail.com>
2023-11-25 15:30:31 +0300

    -t ile topic kaydetme seçeneği geldi sadece ilk sayfada çıkan entryleri alıyor

--- eksi.py
+++ eksi.py
@@ -1,25 +1,19 @@
 import argparse
 import os
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
+
+MAIN_URL = "https://eksisozluk1923.com/"
 
 def init_argparser():
     parser = argparse.ArgumentParser(
         prog="Entry_Saver", description="An entry saver program for Eksi Sözlük"
     )
     return parser
-
-
-parser = init_argparser()
-
 
 def get_arguments():
     try:
@@ -37,28 +31,22 @@ def get_arguments():
             type=str,
             help="You shoul enter a valid author name usage ex: python eksi.py -a mehmetali",
         )
+        parser.add_argument(
+            "-t",
+            "--topic",
+            metavar="TOPİC",
+            type=str,
+            help="You shoul enter a valid eksisözlük topic url and page number usage ex: python eksi.py -t VALİD_URL 10"
+        )
         args = parser.parse_args()
         return args
     except Exception as e:
         print(e)
         raise ValueError
 
-
-# mustafa culcunun son dudugu
-args = get_arguments()
-
-entry_no = str(args.entry_no)
-author = str(args.author)
-
-def create_entry_URL(entry_no):
-    entry_URL = "https://eksisozluk1923.com/entry/"
-    entry_URL = entry_URL + entry_no
-    return entry_URL
-
-def create_author_URL(author):
-    author_URL = "https://eksisozluk1923.com/biri/"
-    author_URL = author_URL + author
-    return author_URL
+def get_final_url(PAGE_TYPE, NAME):
+    types = {"entry":"entry/","author":"biri/"}
+    return str(MAIN_URL) + str(types[PAGE_TYPE]) + str(NAME)
 
 def config_options():
     options = webdriver.ChromeOptions()
@@ -72,70 +60,25 @@ def config_driver(options):
     driver.implicitly_wait(10)
     return driver
 
-
-# control for web page whether open or not
 def open_web_page(URL, driver):
     driver.get(URL)
 
-
 def get_entry_text(driver):
-    entry_text = (
-        WebDriverWait(driver, 20)
-        .until(
-            EC.element_to_be_clickable(
-                (
-                    By.XPATH,
-                    "/html/body/div[2]/div[2]/div[2]/section/div[1]/ul/li/div[1]",
-                )
-            )
-        )
-        .text
-    )
+    entry_text = driver.find_element(By.CLASS_NAME,"content content-expanded").text
     return entry_text
 
-
 def get_entry_title(driver):
-    entry_title = (
-        WebDriverWait(driver, 20)
-        .until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "/html/body/div[2]/div[2]/div[2]/section/div[1]/h1/a/span")
-            )
-        )
-        .text
-    )
+    entry_title = driver.find_element(By.XPATH, "//span[contains(@itemprop, 'name')]").text
+    
     return entry_title
 
-
 def get_entry_date(driver):
-    entry_date = (
-        WebDriverWait(driver, 20)
-        .until(
-            EC.element_to_be_clickable(
-                (
-                    By.XPATH,
-                    "/html/body/div[2]/div[2]/div[2]/section/div[1]/ul/li/footer/div[2]/div/div[1]/div[2]/a",
-                )
-            )
-        )
-        .text
-    )
+    entry_date = driver.find_element(By.CLASS_NAME,"entry-date permalink").text
     return entry_date
 
 
 def get_entry_author(driver):
-    entry_author = (
-        WebDriverWait(driver, 20)
-        .until(
-            EC.element_to_be_clickable(
-                (
-                    By.XPATH,
-                    "/html/body/div[2]/div[2]/div[2]/section/div[1]/ul/li/footer/div[2]/div/div[1]/div[1]/div/a",
-                )
-            )
-        )
-        .text
-    )
+    entry_author = driver.find_element(By.CLASS_NAME,"entry-author").text
     return entry_author
 
 
@@ -180,7 +123,7 @@ def true_page_control(driver):
     try:
         error = driver.find_element(By.CLASS_NAME ,'error404')
         print("bu geçerli bir entry no değil böyle bir sayfa yok")
-        return False
+        return False, error
     except NoSuchElementException:
         pass
     try:
@@ -193,7 +136,7 @@ def true_page_control(driver):
     return True
     
 def entry_option(entry_no):
-    entry_URL = create_entry_URL(entry_no)
+    entry_URL = get_final_url("entry",entry_no)
     options = config_options()
     driver = config_driver(options)
     open_web_page(entry_URL, driver)
@@ -263,6 +206,15 @@ def content_print(author, driver):
     print(all_entries)
     return all_entries
 
+def content_print_topic(driver):
+    content_body = driver.find_elements(By.XPATH, "//ul[@id='entry-item-list']/li")    
+    all_entries =""
+    for i in content_body:
+        current_text = i.text
+        all_entries = all_entries + current_text + "\n\n"
+    print(all_entries)
+    return all_entries
+
 def save_all_entries(author_total_entry_number, driver, author):
     
     if int(author_total_entry_number) == 0:
@@ -291,7 +243,7 @@ def save_author_to_txt(
     file_name = f"{author}.txt"
     dir_path = dir_path + "/saves/authors"
     target_path = os.path.join(dir_path, file_name)
-    yazilar = save_all_entries(author_total_entry_number, driver, author)
+    entries = save_all_entries(author_total_entry_number, driver, author)
     try:
         save_file = open(target_path, "w")
         save_file.write(
@@ -308,7 +260,7 @@ def save_author_to_txt(
             + author_following_number
             + "\n"
             + "Authors all entries:\n\n"
-            + yazilar
+            + entries
             
         )
 
@@ -318,12 +270,8 @@ def save_author_to_txt(
         print("The file was saved succesfully.")
         save_file.close()
 
-  
-
-
-
 def author_option(author):
-    author_URL = create_author_URL(author)
+    author_URL = get_final_url("author",author)
     options = config_options()
     driver = config_driver(options)
     open_web_page(author_URL, driver)
@@ -337,9 +285,74 @@ def author_option(author):
         author_following_number,
         driver,
     )
+    
+def get_topic(driver):
+    tittle = (
+        WebDriverWait(driver, 20)
+        .until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//span[contains(@itemprop, 'name')]",
+                )
+            )
+        )
+        .text
+    )
+    return tittle
 
-if((args.entry_no) != None):
-    entry_option(entry_no)
+def save_topics_first_page(topic, driver):
+    dir_path = os.getcwd()
 
-if((args.author) != None):
-    author_option(author)
+    saves_topics_klasoru = os.path.join(dir_path, "saves/topics")
+
+    if not os.path.exists(saves_topics_klasoru):
+        os.makedirs(saves_topics_klasoru)
+        print(f"'saves/topics' klasörü {dir_path} dizininde oluşturuldu.")
+    else:
+        print(f"'saves/topics' klasörü zaten {dir_path} dizininde mevcut.")
+
+    file_name = f"{topic}.txt"
+    dir_path = dir_path + "/saves/topics"
+    target_path = os.path.join(dir_path, file_name)
+    entries = content_print_topic(driver)
+
+    try:
+        save_file = open(target_path, "w")
+        save_file.write(
+            "Topic: "
+            + topic
+            + "\n\n\n"
+            + "Topics: "
+            + entries
+        )
+
+    except IOError:
+        print("the file was couldn't find or read.")
+    else:
+        print("The file was saved succesfully.")
+        save_file.close()
+
+def topic_option(topic_url):
+    options=config_options()
+    driver=config_driver(options)
+    open_web_page(topic_url, driver)
+    topic=get_topic(driver)
+    save_topics_first_page(topic, driver)
+        
+
+
+if __name__=="__main__":
+    parser = init_argparser()
+    args = get_arguments()
+
+    if((args.entry_no) != None):
+        entry_option(str(args.entry_no))
+
+    if((args.author) != None):
+        author_option(str(args.author))
+        
+    if((args.topic) != None):
+        topic_option(str(args.topic))
+        
+    
